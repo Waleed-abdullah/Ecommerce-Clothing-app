bc5b291631b593c9aea7a16d82a6f64c23e06a07
Waleed-Abdullah <w26waleed@gmail.com>
2021-11-17 23:08:36 +0500

    added axios

--- src/components/ProfileInfo/SaveProfileButton.js
+++ src/components/ProfileInfo/SaveProfileButton.js
@@ -1,38 +1,49 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Button from '@mui/material/Button';
 import { useStateValue } from '../../State/StateProvider';
 
-
 const useStyles = makeStyles({
-    button: {
-      backgroundColor: '#804FC0',
+  button: {
+    backgroundColor: '#804FC0',
+    color: '#fff',
+    '&:hover': {
+      backgroundColor: '#6c3da9 !important',
       color: '#fff',
-      '&:hover': {
-        backgroundColor: '#6c3da9 !important',
-        color: '#fff',
     },
-}})
+  },
+});
 
+const SaveProfileButton = ({ name, region }) => {
+  const classes = useStyles();
+  //eslint-disable-next-line
+  const [{ user }, dispatch] = useStateValue();
 
+  const handleClick = () => {
+    console.log(user);
+  };
 
-const SaveProfileButton = ({name, region}) => {
-    const classes = useStyles()
-    const [{user}, dispatch] = useStateValue()
+  return (
+    <Button
+      className={classes.button}
+      type="submit"
+      onClick={handleClick}
+      variant="text"
+      size="large"
+      sx={{
+        bgcolor: '#804FC0',
+        width: '100%',
+        color: '#ffffff',
+        mt: 1,
+        borderRadius: '15px',
+        fontWeight: 'bold',
+        fontSize: 'h6.fontSize',
+        boxShadow: 10,
+      }}
+    >
+      Save Profile
+    </Button>
+  );
+};
 
-    const handleClick = () => {
-        console.log(user)
-    }
-
-    return (
-        <Button className={classes.button}
-        type='submit'
-        variant="text"
-        size="large"
-        sx={{bgcolor: "#804FC0", width:"100%", color:"#ffffff", mt:1, borderRadius: '15px', fontWeight: "bold", fontSize: "h6.fontSize", boxShadow: 10}}>
-        Save Profile
-        </Button>
-    )
-}
-
-export default SaveProfileButton
+export default SaveProfileButton;
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -5,6 +5,7 @@ import { auth, provider } from '../../firebase';
 import { useStateValue } from '../../State/StateProvider';
 import { actionTypes } from '../../State/Reducer';
 import SignInButton from './SigninButton';
+import axios from 'axios';
 
 const Signin = () => {
   const [{ user }, dispatch] = useStateValue();
@@ -15,7 +16,7 @@ const Signin = () => {
       .signInWithPopup(provider)
       .then((result) => {
         const temp = result.user.multiFactor.user;
-        console.log(temp)
+        console.log(temp);
 
         // sends data/state to the store
         dispatch({
@@ -29,7 +30,23 @@ const Signin = () => {
           },
         });
       })
-      .catch((error) => console.log(error.message));
+      .catch((error) => console.log(error.message))
+      .then(() => {
+        ApiCall();
+      });
+  };
+
+  //api call to server
+
+  const ApiCall = () => {
+    console.log('Data being sent', user);
+    axios({
+      method: 'post',
+      url: 'http://localhost:5000/signin',
+      data: {
+        ...user,
+      },
+    }).then((res) => console.log(res));
   };
 
   return (
